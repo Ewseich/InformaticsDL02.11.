a85c48e1391edca2f5807ae7e04661bb0f84f4ac
Ewseich <timapoton@gmail.com>
2024-10-30 22:37:05 +0300

    end?

---
+++ src/task116.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class task116 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int srav;
+        int min = 9;
+        int max = 0;
+        while (a > 0) {
+            srav = a % 10;
+            if (srav < min) {
+                min = srav;
+            }
+            if (srav > max) {
+                max = srav;
+            }
+            a /= 10;
+        }
+        System.out.println(min+ " " +max);
+    }
+}
