267d4493398426191324306dd3fcd96ba6ecef61
Ewseich <timapoton@gmail.com>
2024-10-30 20:02:27 +0300

    end?

---
+++ src/task27.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class task27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int aa = a;
+        int b = sc.nextInt();
+        int bb = b;
+        while (a * b != 0) {
+            int s = Math.min(a,b);
+            b = Math.max(a,b) % Math.min(a,b);
+            a = Math.abs(s);
+        }
+        if (a == 0) {
+            a = bb;
+        }
+        int c = aa / a;
+        int d = bb / a;
+        System.out.println(c + " " + d);
+
+
+    }
+}
