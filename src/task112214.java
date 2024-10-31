c7cee67c4f504c688a2ed6cf45089698aa5a51b8
Ewseich <timapoton@gmail.com>
2024-10-31 22:04:50 +0300

    end?

---
+++ src/task112214.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class task112214 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int last = -1;
+        int yes = 0;
+        while (n > 0) {
+            if (n % 10 == last) {
+                System.out.println("YES");
+                yes += 1;
+                break;
+            }
+            else {
+                last = n % 10;
+            }
+            n /= 10;
+        }
+        if (yes == 0) {
+            System.out.println("NO");
+        }
+    }
+}
