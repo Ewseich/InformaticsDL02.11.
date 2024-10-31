4b304eecd286f472a357d1a98699a0b9d91cc13f
Ewseich <timapoton@gmail.com>
2024-10-31 22:21:57 +0300

    end?

---
+++ src/task1422.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class task1422 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long nn = n;
+        long k = sc.nextInt();
+        long kk = k;
+        while (n * k != 0) {
+            long s = Math.min(n, k);
+            n = Math.max(n, k) % Math.min(n, k);
+            k = Math.abs(s);
+        }
+        System.out.println((nn * kk) / k);
+    }
+
+}
