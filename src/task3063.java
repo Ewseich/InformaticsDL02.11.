83ecdabab5767f12ccbc96f552fa9a467fe4fcba
Ewseich <timapoton@gmail.com>
2024-10-31 20:08:40 +0300

    end?

---
+++ src/task3063.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class task3063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        int p = sc.nextInt();
+        int y = sc.nextInt();
+        int year = 0;
+        while (x < y) {
+            x += x / 100 * p;
+            x = (int)(x * 100);
+            x /= 100;
+            year += 1;
+        }
+        System.out.println((int)year);
+
+    }
+}
