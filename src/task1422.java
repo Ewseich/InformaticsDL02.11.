b904b17cf1afafacd25406e07f2ca1daa129402e
Ewseich <timapoton@gmail.com>
2024-10-31 22:39:54 +0300

    end?

--- src/task1422.java
+++ src/task1422.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner;//измените переменную
 public class task1422 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- src/task27.java
+++ src/task27.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner;//измените переменную
 
 import static java.lang.System.exit;
 
